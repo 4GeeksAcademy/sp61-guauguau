591c7fcae7b4b395280685e666d63e146738f905
Juan Antonio Cortes Elrio <152733103+Mogurkazan@users.noreply.github.com>
2024-05-24 13:57:07 +0000

    add pets page

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -10,6 +10,7 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { Pets } from "./pages/pets";
 
 //create your first component
 const Layout = () => {
@@ -27,6 +28,7 @@ const Layout = () => {
                     <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
+                        <Route element={<Pets/>} path="/pets" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
