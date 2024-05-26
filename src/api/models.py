84d4fee4a0944db42dea06405d572951a1b8ea95
Estrella Benítez Buitrago <156102986+Llitabb@users.noreply.github.com>
2024-05-26 15:28:01 +0000

    creado componente City

--- src/api/models.py
+++ src/api/models.py
@@ -52,10 +52,9 @@ class Pet(db.Model):
         return f'<Pet {self.name}>'
     
 class City(db.Model):
-    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), nullable=False)
-    pet_friendly = db.Column(db.String(250), nullable=False)
+    pet_friendly = db.Column(db.String(80), nullable=False)
 
     def __repr__(self):
         return f'<City {self.name}>'
