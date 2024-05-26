41d29f02f8aef8dac16ce1175776691ac2b9c4a6
Cristina Machuca <132057230+CrisMachuca@users.noreply.github.com>
2024-05-26 17:47:16 +0000

    arreglo errores flux, todo funciona

--- src/front/js/store/appContext.js
+++ src/front/js/store/appContext.js
@@ -20,8 +20,8 @@ const injectContext = PassedComponent => {
 					})
 			})
 		);
-        useEffect(() => {
-            
+
+		useEffect(() => {
 			/**
 			 * EDIT THIS!
 			 * This function is the equivalent to "window.onLoad", it only runs once on the entire application lifetime
