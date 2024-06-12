fe49471eeb38f110416978de80359874d817510f
Cristina Machuca <132057230+CrisMachuca@users.noreply.github.com>
2024-06-12 23:46:01 +0000

    diseño de singlePet

--- migrations/versions/516aa9858bd7_.py
+++ migrations/versions/516aa9858bd7_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 78318accb3b6
+Revision ID: 516aa9858bd7
 Revises: 
-Create Date: 2024-06-12 20:49:06.383979
+Create Date: 2024-06-12 22:47:33.934427
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '78318accb3b6'
+revision = '516aa9858bd7'
 down_revision = None
 branch_labels = None
 depends_on = None
