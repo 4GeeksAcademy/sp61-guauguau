ea4f40a88a5da95f7ee10412c769189dad2d88ce
Cristina Machuca <132057230+CrisMachuca@users.noreply.github.com>
2024-06-12 21:02:53 +0000

    doy estilo a singlePet

--- src/front/js/pages/singlePet.js
+++ src/front/js/pages/singlePet.js
@@ -1,5 +1,4 @@
 import "../../styles/singlepet.css";
-
 import React, { useState, useEffect, useContext } from 'react';
 import { Context } from '../store/appContext';
 import { useParams, Link, useNavigate } from 'react-router-dom';
@@ -119,44 +118,10 @@ export const SinglePet = () => {
         <div className="container single-pet-container">
             <div className="pet-card">
                 <div className="pet-card-header">
-                    <h2>{petDetails.name}, {petDetails.age} years</h2>
-                    <div className="owner-info">
-                        {petDetails.ownerPhoto && (
-                            <Link to={`/singleowner/${petDetails.ownerId}`}>
-                                <img src={petDetails.ownerPhoto} alt="Owner Profile" className="owner-photo" />
-                            </Link>
-                        )}
-                        <span>{petDetails.owner}</span>
-                    </div>
-                </div>
-                <div className="pet-card-body">
+                    <h1>{petDetails.name}, {petDetails.age} years</h1>
                     {petDetails.profile_photo_url && (
                         <img src={petDetails.profile_photo_url} alt="Pet Profile" className="pet-profile-photo" />
                     )}
-                    <div className="pet-details">
-                        <div className="detail-item">
-                            <label>Breed:</label>
-                            <span>{petDetails.breed}</span>
-                        </div>
-                        <div className="detail-item">
-                            <label>Sex:</label>
-                            <span>{petDetails.sex}</span>
-                        </div>
-                        <div className="detail-item">
-                            <label>City:</label>
-                            <span>{petDetails.city}</span>
-                        </div>
-                        <div className="detail-item">
-                            <label>Pedigree:</label>
-                            <span>{petDetails.pedigree ? 'Yes' : 'No'}</span>
-                        </div>
-                        <div className="detail-item-description">
-                            <label>About me:</label>
-                            <span>{petDetails.description}</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="pet-card-footer">
                     {store.auth && (
                         <div className="like-section">
                             <select
@@ -174,108 +139,83 @@ export const SinglePet = () => {
                             </button>
                         </div>
                     )}
+                    <div className="additional-photos">
+                        {petDetails.photos.slice(0, 4).map((photo, index) => (
+                            <img src={photo.url} alt={`Pet ${index}`} className="additional-photo" key={index} />
+                        ))}
+                    </div>
+                </div>
+                <div className="pet-card-body">
+                    <div className="detail-item-description">
+                        <p>{petDetails.description}</p>
+                    </div>
+                    <div className="pet-details">
+                        <div className="detail-item">
+                            <label>Breed:</label>
+                            <span>{petDetails.breed}</span>
+                        </div>
+                        <div className="detail-item">
+                            <label>Sex:</label>
+                            <span>{petDetails.sex}</span>
+                        </div>
+                        <div className="detail-item">
+                            <label>City:</label>
+                            <span>{petDetails.city}</span>
+                        </div>
+                        <div className="detail-item">
+                            <label>Pedigree:</label>
+                            <span>{petDetails.pedigree ? 'Yes' : 'No'}</span>
+                        </div>
+                    </div>
+                    <div className="owner-info">
+                        {petDetails.ownerPhoto && (
+                            <Link to={`/singleowner/${petDetails.ownerId}`}>
+                                <img src={petDetails.ownerPhoto} alt="Owner Profile" className="owner-photo" />
+                            </Link>
+                        )}
+                        <span>{petDetails.owner}</span>
+                    </div>
                 </div>
             </div>
             <div className="additional-section">
-                <div className="additional-text"><h3>Additional pictures</h3></div>
-                <div className="additional-photos">
-                    {petDetails.photos.map((photo, index) => (
-                        <img src={photo.url} alt={`Pet ${index}`} className="additional-photo" key={index} />
-                    ))}
-                </div>
                 <div className="info-buttons">
-                    <button className="btn  me-2 ia-btn" onClick={fetchCareInfo}>Cuidados </button>
-                    <button className="btn  me-2 ia-btn" onClick={fetchCompatibilityInfo}>Compatibilidad </button>
+                    <button className="btn me-2 btn-multicolor info-buttons" onClick={fetchCareInfo} disabled={isLoadingCareInfo}>
+                        {isLoadingCareInfo ? 'Loading...' : 'Cares'}
+                    </button>
+                    <button className="btn me-2 btn-multicolor info-buttons" onClick={fetchCompatibilityInfo} disabled={isLoadingCompatibilityInfo}>
+                        {isLoadingCompatibilityInfo ? 'Loading...' : 'Compatibility'}
+                    </button>
                 </div>
                 {careInfo && (
                     <div className="care-info">
-                        <h3>Cuidados</h3>
+                        <h3>Cares</h3>
                         <p>{careInfo}</p>
                     </div>
                 )}
                 {compatibilityInfo && (
                     <div className="compatibility-info">
-                        <h3>Compatibilidad</h3>
+                        <h3>Compatibility</h3>
                         <p>{compatibilityInfo}</p>
                     </div>
                 )}
-                <Link to="/pets" className="btn btn-secondary back-button">
+                <h3 className='p-5 ps-0'>Matches</h3>
+                <div className="row">
+                    {matches.map((match, index) => (
+                        <div className="col-md-3 mb-3" key={index}>
+                            <div className="d-flex align-items-center">
+                                <img src={match.match_pet_photo} alt={match.match_pet_name} className="img-fluid rounded-circle me-2" style={{ width: '50px', height: '50px' }} />
+                                <span>{match.match_pet_name}</span>
+                                <button className="btn btn-secondary ms-2" onClick={() => navigate(`/chat/${petId}/${match.match_pet_id}`)}>
+                                    <i className="fas fa-comments"></i> Chat
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <Link to="/pets" className="btn btn-secondary mt-3">
                     <i className="fas fa-arrow-left"></i> Back to the list of pets
                 </Link>
             </div>
-            <button className="btn btn-secondary me-2" onClick={fetchCareInfo} disabled={isLoadingCareInfo}>
-                {isLoadingCareInfo ? (
-                    <div aria-label="Orange and tan hamster running in a metal wheel" role="img" className="wheel-and-hamster">
-                        <div className="wheel"></div>
-                        <div className="hamster">
-                            <div className="hamster__body">
-                                <div className="hamster__head">
-                                    <div className="hamster__ear"></div>
-                                    <div className="hamster__eye"></div>
-                                    <div className="hamster__nose"></div>
-                                </div>
-                                <div className="hamster__limb hamster__limb--fr"></div>
-                                <div className="hamster__limb hamster__limb--fl"></div>
-                                <div className="hamster__limb hamster__limb--br"></div>
-                                <div className="hamster__limb hamster__limb--bl"></div>
-                                <div className="hamster__tail"></div>
-                            </div>
-                        </div>
-                        <div className="spoke"></div>
-                    </div>
-                ) : 'Cuidados'}
-            </button>
-            {careInfo && (
-                <div>
-                    <h3>Cuidados</h3>
-                    <p>{careInfo}</p>
-                </div>
-            )}
-            <button className="btn btn-secondary me-2" onClick={fetchCompatibilityInfo} disabled={isLoadingCompatibilityInfo}>
-                {isLoadingCompatibilityInfo ? (
-                    <div aria-label="Orange and tan hamster running in a metal wheel" role="img" className="wheel-and-hamster">
-                        <div className="wheel"></div>
-                        <div className="hamster">
-                            <div className="hamster__body">
-                                <div className="hamster__head">
-                                    <div className="hamster__ear"></div>
-                                    <div className="hamster__eye"></div>
-                                    <div className="hamster__nose"></div>
-                                </div>
-                                <div className="hamster__limb hamster__limb--fr"></div>
-                                <div className="hamster__limb hamster__limb--fl"></div>
-                                <div className="hamster__limb hamster__limb--br"></div>
-                                <div className="hamster__limb hamster__limb--bl"></div>
-                                <div className="hamster__tail"></div>
-                            </div>
-                        </div>
-                        <div className="spoke"></div>
-                    </div>
-                ) : 'Compatibilidad'}
-            </button>
-            {compatibilityInfo && (
-                <div>
-                    <h3>Compatibilidad</h3>
-                    <p>{compatibilityInfo}</p>
-                </div>
-            )}
-            <h3 className='p-5 ps-0'>Matches</h3>
-            <div className="row">
-                {matches.map((match, index) => (
-                    <div className="col-md-3 mb-3" key={index}>
-                        <div className="d-flex align-items-center">
-                            <img src={match.match_pet_photo} alt={match.match_pet_name} className="img-fluid rounded-circle me-2" style={{ width: '50px', height: '50px' }} />
-                            <span>{match.match_pet_name}</span>
-                            <button className="btn btn-secondary ms-2" onClick={() => navigate(`/chat/${petId}/${match.match_pet_id}`)}>
-                                <i className="fas fa-comments"></i> Chat
-                            </button>
-                        </div>
-                    </div>
-                ))}
-            </div>
-            <Link to="/pets" className="btn btn-secondary mt-3">
-                <i className="fas fa-arrow-left"></i> Back to the list of pets
-            </Link>
         </div>
     );
 };
