d71e6fc37f20a07004900def7df1b149e3f0b2ef
Juan Antonio Cortes Elrio <152733103+Mogurkazan@users.noreply.github.com>
2024-06-13 15:09:15 +0000

    deploy test

--- migrations/versions/109ccfaaba4f_.py
+++ migrations/versions/109ccfaaba4f_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 516aa9858bd7
+Revision ID: 109ccfaaba4f
 Revises: 
-Create Date: 2024-06-12 22:47:33.934427
+Create Date: 2024-06-13 15:07:32.330017
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '516aa9858bd7'
+revision = '109ccfaaba4f'
 down_revision = None
 branch_labels = None
 depends_on = None
