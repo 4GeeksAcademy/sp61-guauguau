d7f1eb64260b2066b19ca6b37fc664cf2e23037c
Estrella Benítez Buitrago <156102986+Llitabb@users.noreply.github.com>
2024-06-14 02:31:58 +0000

    Añado cambios a Team

--- src/front/js/pages/team.js
+++ src/front/js/pages/team.js
@@ -1,32 +1,42 @@
 import React from 'react';
 import '../../styles/home.css'; // Asegúrate de que esta ruta sea correcta
-import Juan from "../../img/juan.jpg"
-
+import Juan from "/public/juan.jpg";
+import Estrella from "/public/estrella.jpg";
+import Pedro from "/public/pedro.jpg";
+import Cris from "/public/fotoCris.jpg";
 
 const teamMembers = [
     {
         name: "Juan",
-        imgSrc: Juan // Reemplaza con la ruta correcta
+        imgSrc: Juan,
+        github:"https://github.com/Mogurkazan",
+        linkedin:"https://www.linkedin.com/in/juan-antonio-cortes-elrio-a6a58145/",
     },
     {
         name: "Estrella",
-        imgSrc: "https://img.freepik.com/foto-gratis/mujer-hermosa-joven-mirando-camara-chica-moda-verano-casual-camiseta-blanca-pantalones-cortos-hembra-positiva-muestra-emociones-faciales-modelo-divertido-aislado-amarillo_158538-15796.jpg?t=st=1717893953~exp=1717897553~hmac=c5066d3aa8ec3166728e4a2946c6f65eb8b943afe4459edb36f1513e891652ca&w=1380" // Reemplaza con la ruta correcta
+        imgSrc: Estrella,
+        github:"https://github.com/Llitabb",
+        linkedin:"https://www.linkedin.com/in/estrella-benítez-buitrago/",
     },
     {
         name: "Pedro",
-        imgSrc: "https://img.freepik.com/foto-gratis/joven-barbudo-camisa-rayas_273609-5677.jpg?t=st=1717894038~exp=1717897638~hmac=8773ff51e02780af286624c6d7aac08e1ae181f2e0ba0e669ec74639a6cb7241&w=1800" // Reemplaza con la ruta correcta
+        imgSrc: Pedro,
+        github:"https://github.com/jppe1994",
+        linkedin:"https://www.linkedin.com/in/pedro-abel-mendoza-alcivar-2545bba7",
     },
     {
         name: "Cris",
-        imgSrc: "https://img.freepik.com/foto-gratis/chica-agradable-cabello-castano-brillante-sonriendo-foto-interior-dama-caucasica-pie-brazos-cruzados_197531-9395.jpg?t=st=1717893991~exp=1717897591~hmac=021871108b29ed3f88e0576cc0599c837b25aea01bdff971bede33e2926b97bb&w=1800" // Reemplaza con la ruta correcta
+        imgSrc: Cris,
+        github: "https://github.com/CrisMachuca",
+        linkedin: "https://www.linkedin.com/in/cristina-machuca-martínez-5636b2274/",
     }
 ];
 
 export const Team = () => {
     return (
-        <div className="container-fluid team-container">
-            <div className="team-row">
-                <h1 className='mt-3 p-3'>Meet the developers</h1>
+        <div className="container team-container">
+            <div className="team-header">
+                <h1 className='mt-3 p-3'>Know the developers</h1>
             </div>
             <div className="team-row">
                 {teamMembers.map((member, index) => (
@@ -35,10 +45,17 @@ export const Team = () => {
                             <img src={member.imgSrc} alt={member.name} className="team-img"/>
                         </div>
                         <h4 className="team-name">{member.name}</h4>
+                        <div className="team-links">
+                            <a href={member.github} className='team-github' target="_blank" rel="noopener noreferrer">
+                                <i className="fab fa-github"></i>
+                            </a>
+                            <a href={member.linkedin} className='team-linkedin' target="_blank" rel="noopener noreferrer">
+                                <i className="fab fa-linkedin"></i>
+                            </a>
+                        </div>
                     </div>
                 ))}
             </div>
-           
         </div>
     );
 };
